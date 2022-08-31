98beae62edeee12b997c37fd4443375a9b89e5e7
gustavojb1 <gustavojunqueirab@gmail.com>
2022-08-31 14:53:34 -0300

    exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+let troca =frase.replaceAll("verde","rosa")
+
+troca=troca.replaceAll("azul","laranja")
+
+let maiuscula= troca.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO CAIR")
+
+console.log(troca)
+
+console.log(maiuscula)
